843b18e246e548cea3140bd5a2bbc5d849e880e1
timbaron <timothy.akiode@student.aul.edu.ng>
2023-05-10 11:41:26 +0100

    project setup and top section

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Unleash your creative potential with PromptHub, the ultimate
+        AI-powered prompt discovery and sharing platform. Discover captivating
+        prompts, create your own, and connect with a vibrant community of
+        like-minded creators. Elevate your creativity today with PromptHub.
+      </p>
+      {/* Feeds */}
+    </section>
+  );
+};
+
+export default Page;
