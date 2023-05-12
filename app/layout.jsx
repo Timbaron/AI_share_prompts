4a6b829135717fa113184b6465f13961eb2b6b00
timbaron <timothy.akiode@student.aul.edu.ng>
2023-05-12 10:42:55 +0100

    setting up nav bar and initializing auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,25 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "PromptHub",
   description: "Discover and Share AI prompt",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
             {children}
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
